885652cb72f587384dff9906b95064e09b173a74
Ayush Sahu <ayush.cbs1@gmail.com>
2021-07-05 18:07:01 +0530

    Add files via upload

--- Content.js
+++ Content.js
@@ -1,1 +1,6 @@
-alert("Is this the best use of your time?");
+alert('Sure you wanna do this?');
+	x = Math.floor(Math.random() * 1000);
+	y = Math.floor(Math.random() * 1000);
+	let pass = prompt(`What is ${x} + ${y} ?`);
+	while(pass != x+y){ prompt(`What is ${x} + ${y} ?`); }
+    alert('You can proceed.');
